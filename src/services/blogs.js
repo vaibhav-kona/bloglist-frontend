a6fdf86df9d409449af6c767133a8d7cd899af1e
Vaibhav <vaibhav@bluesapling.com>
2020-11-02 19:07:39 +0530

    Make a skeleton code for login

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,22 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+
+const baseUrl = '/api/blogs';
+
+let token = '';
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-export default { getAll }
+const create = async (newBlog) => {
+  const config = { headers: { Authorization: token } };
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
+
+export default { create, getAll, setToken };
